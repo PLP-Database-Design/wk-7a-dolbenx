0677510281cd36146a78c2413e1f9f517f7007a9
dolbenx <58547915+dolbenx@users.noreply.github.com>
2025-05-05 10:28:50 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -16,29 +16,17 @@ SELECT
 FROM SplitProducts;
 
 2.
--- Create a table to store OrderID and CustomerName
-CREATE TABLE Orders (
-    OrderID INT PRIMARY KEY,
-    CustomerName VARCHAR(100)
-);
 
--- Insert data into the Orders table
-INSERT INTO Orders (OrderID, CustomerName)
-SELECT DISTINCT OrderID, CustomerName
+-- First Orders Table (OrderID → CustomerName) --
+SELECT DISTINCT 
+    OrderID, 
+    CustomerName
+FROM OrderDetails;
+ --Second OrderItems Table (OrderID + Product → Quantity) --
+SELECT 
+    OrderID, 
+    Product, 
+    Quantity
 FROM OrderDetails;
 
-
--- Create a table to store OrderID, Product, and Quantity
-CREATE TABLE OrderItems (
-    OrderID INT,
-    Product VARCHAR(100),
-    Quantity INT,
-    PRIMARY KEY (OrderID, Product),  -- Composite primary key
-    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
-);
-
--- Insert data into the OrderItems table
-INSERT INTO OrderItems (OrderID, Product, Quantity)
-SELECT OrderID, Product, Quantity
-FROM OrderDetails;
 
