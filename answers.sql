f7d0b339a8b8a1f50235b18dd9f8d847dbddaab5
dolbenx <58547915+dolbenx@users.noreply.github.com>
2025-05-16 11:12:06 +0200

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,32 +1,46 @@
 1.
-WITH SplitProducts AS (
-    SELECT 
-        OrderID,
-        CustomerName,
-        JSON_TABLE(
-            CONCAT('["', REPLACE(Products, ',', '","'), '"]'),
-            '$[*]' COLUMNS (Product VARCHAR(100) PATH '$')
-        ) AS jt
-    FROM ProductDetail
-)
-SELECT 
-    OrderID,
-    CustomerName,
-    jt.Product
-FROM SplitProducts;
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, 'John Doe', 'Laptop'),
+(101, 'John Doe', 'Mouse'),
+(102, 'Jane Smith', 'Tablet'),
+(102, 'Jane Smith', 'Keyboard'),
+(102, 'Jane Smith', 'Mouse'),
+(103, 'Emily Clark', 'Phone');
 
 2.
+CREATE TABLE orders(
+OrderID INT PRIMARY KEY,
+customerName VARCHAR(100)
+);
+INSERT INTO orders (OrderID, CustomerName)
+VALUES
+(101, 'John Doe'),
+(102, 'Jane Smith'),
+(103, 'Emily Clark');
 
--- First Orders Table (OrderID → CustomerName) --
-SELECT DISTINCT 
-    OrderID, 
-    CustomerName
-FROM OrderDetails;
- --Second OrderItems Table (OrderID + Product → Quantity) --
-SELECT 
-    OrderID, 
-    Product, 
-    Quantity
-FROM OrderDetails;
+-- Product  table 
+CREATE TABLE product(
+product_id INT primary key,
+productName varchar(100),
+quantity INT,
+order_id INT,
+foreign key(order_id) references orders(OrderID)
+);
+
+insert into product(product_id,productName,quantity,order_id)
+values 
+(1,'laptop',2,101),
+(2,'Mouse',1,101),
+(3,'Tablet',3,102),
+(4,'Keyboard',2,102),
+(5,'Mouse',1,102),
+(6,'Phone',1,103);
+
 
 
